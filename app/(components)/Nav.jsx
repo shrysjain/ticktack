c479053c36dfa8052a4ea027324e23d3e7ceeebc
Shreyas Jain <shrys.jain@gmail.com>
2024-07-14 20:17:31 -0400

    commit

---
+++ app/(components)/Nav.jsx
@@ -0,0 +1,31 @@
+import {
+  faHome,
+  faRightFromBracket,
+  faTicket,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+
+const Nav = () => {
+  return (
+    <nav className="flex justify-between bg-nav p-4">
+      <div className="flex items-center space-x-4">
+        <Link href="/">
+          <FontAwesomeIcon icon={faHome} className="icon" />
+        </Link>
+        <Link href="/tickets/new">
+          <FontAwesomeIcon icon={faTicket} className="icon" />
+        </Link>
+      </div>
+      <div className="flex space-x-3 items-center">
+        <p className="text-default-text">
+          Logged in as <b>shrysjain</b>
+          {/* Eventually implement proper sign-in w/ Google OAuth */}
+        </p>
+        <FontAwesomeIcon icon={faRightFromBracket} className="icon" />
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
---
+++ app/tickets/[id]/page.jsx
@@ -0,0 +1,5 @@
+const tickets = ({ params }) => {
+  return <div>Ticket #{params.id}</div>;
+};
+
+export default tickets;
