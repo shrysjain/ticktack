5ec583b9e74a38797be660ac4fe45a8ac57fa89e
Shreyas Jain <shrys.jain@gmail.com>
2024-07-15 03:09:10 -0400

    typo

--- app/tickets/[id]/page.jsx
+++ app/tickets/[id]/page.jsx
@@ -1,4 +1,4 @@
-import TicketForm from "@/app/(components)/TicketFOrm";
+import TicketForm from "@/app/(components)/TicketForm";
 
 const getTicketById = async (id) => {
   const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
